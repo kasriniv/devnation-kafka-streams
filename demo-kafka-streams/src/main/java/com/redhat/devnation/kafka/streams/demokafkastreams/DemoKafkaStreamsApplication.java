b1bd33afc69749053b2202ee91df137166b32812
Kavitha Srinivasan <kasriniv@users.noreply.github.com>
2018-12-14 08:45:16 -0500

    Update DemoKafkaStreamsApplication.java

--- demo-kafka-streams/src/main/java/com/redhat/devnation/kafka/streams/demokafkastreams/DemoKafkaStreamsApplication.java
+++ demo-kafka-streams/src/main/java/com/redhat/devnation/kafka/streams/demokafkastreams/DemoKafkaStreamsApplication.java
@@ -86,7 +86,7 @@ public class DemoKafkaStreamsApplication {
 		//ORIGINAL KStream<String, Long> wordCountsStream = wordCounts.toStream();
 		KStream<String, String> wordCountsStream = wordCounts.toStream();
 		//ORIGINAL wordCountsStream.to("streams-wordcount-output", Produced.with(Serdes.String(), Serdes.Long()));
-	wordCountsStream.to("streams-wordcount-output", Produced.with(Serdes.String(), Serdes.String()));
+	wordCountsStream.to("streams-wordcount-output2", Produced.with(Serdes.String(), Serdes.String()));
 		return wordCountsStream;
 	}
 
